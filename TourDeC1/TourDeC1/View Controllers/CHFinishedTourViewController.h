6aa356cb53e63cedd5eecd75191bd108b6d4867d
Anuradha Ramprakash <anuradha.ramprakash@chaione.com>
2014-03-28 18:34:52 -0500

    More UI changes

---
+++ TourDeC1/TourDeC1/View Controllers/CHFinishedTourViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHFinishedTourViewController.h
+//  TourDeC1
+//
+//  Created by Anuradha Ramprakash on 3/28/14.
+//  Copyright (c) 2014 ChaiONE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHFinishedTourViewController : UIViewController
+
+@end
