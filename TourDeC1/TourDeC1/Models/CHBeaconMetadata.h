271db3abff2979a04cb31e65ce2655c361a21ac7
Anuradha Ramprakash <anuradha.ramprakash@chaione.com>
2014-04-07 11:40:32 -0500

    FIxed bugs

--- TourDeC1/TourDeC1/Models/CHBeaconMetadata.h
+++ TourDeC1/TourDeC1/Models/CHBeaconMetadata.h
@@ -14,8 +14,7 @@
 @property (nonatomic, strong) NSString *name;
 @property (nonatomic, strong) NSString *udid;
 @property (nonatomic, strong) NSString *nextBeaconName;
-@property (nonatomic, strong) NSString *nextSegment;
-@property (nonatomic, strong) NSString *nextBeaconUDID;
+@property (nonatomic, strong) NSString *locationOnMapImageName;
 
 - (instancetype)initWithData:(NSDictionary *)data;
 @end
